b96aa20d33be04904de441f61ef10f3b50aa8932
ed_maldo <ed_maldo@example.com>
2021-03-07 07:54:57 -0500

    user can sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./App.css"
 import CreatePost from "./components/CreatePost"
 import DisplayPosts from "./components/DispalyPosts"
+import { withAuthenticator } from "aws-amplify-react"
 
 function App() {
   return (
@@ -12,4 +13,4 @@ function App() {
   )
 }
 
-export default App
+export default withAuthenticator(App, { includeGreetings: true })
