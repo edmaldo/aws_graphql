40e5e8c508268f208a34d4235961b393efe5f310
ed_maldo <ed_maldo@example.com>
2021-03-07 05:02:20 -0500

    added API graphql

--- src/components/DispalyPosts.js
+++ src/components/DispalyPosts.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react"
+import { listPosts } from "../graphql/queries"
+import { API, graphqlOperation } from "aws-amplify"
 
 class DisplayPosts extends Component {
+  componentDidMount = async () => {
+    this.getPosts()
+  }
+
+  getPosts = async () => {
+    const result = await API.graphql(graphqlOperation(listPosts))
+    console.log(JSON.stringify(result.data.listPosts.items))
+  }
   render() {
     return <div>Hello World</div>
   }
